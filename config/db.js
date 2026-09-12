7a6a0a3804354802b7dbb96f2d8891ae13f81f5b
psalmkheed <psalmkheed@gmail.com>
2026-09-12 18:17:41 +0100

    database added

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.COMPASS_URL)
-.then(()=> console.log("Database connected successfully"))
-.catch((error)=> console.log("Connection Error ", error))
+async function startServer() {
+      await mongoose.connect(process.env.MONGODB_URI)
+            .then(() => console.log("Database connected successfully"))
+            .catch((error) => console.log("Connection Error ", error));
+}
 
+startServer();
 module.exports = mongoose;
