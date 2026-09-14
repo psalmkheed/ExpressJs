e0b89ffa7fdd46c3d3073f2ac8d6b23e427890a1
psalmkheed <psalmkheed@gmail.com>
2026-09-14 23:14:03 +0100

    Login, Registration & Dashboard pages updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
       res.sendFile(path.join(__dirname, "public", "register.html"));
 });
-app.get("/dashboard", authenticate, (req, res) => {
+app.get("/dashboard", (req, res) => {
       res.sendFile(path.join(__dirname, "public", "dashboard.html"));
 });
 
