945178c07985b072ac6101cb96f30a52c5b5426f
psalmkheed <psalmkheed@gmail.com>
2026-09-25 23:02:27 +0100

    404 page added

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ app.use('/api/users', userRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/products', productRoutes);
 
+app.use((req, res) => {
+      res.status(404).sendFile(
+            path.join(__dirname, "public", "404.html")
+      );
+})
+
 app.listen(PORT, ()=>{
       console.log(`Server running on http://localhost:${PORT}`)
 });
